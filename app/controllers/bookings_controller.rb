48b3ebf067161ea2ec4bae1179e24261e2921dfa
Shawkat Shami <shawkat.shami7@gmail.com>
2018-11-08 14:06:31 +0800

    create booking update & new booking form

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,12 +2,12 @@ class BookingsController < ApplicationController
 
   def new
     @coworking_booking = Booking.new
-    @coworking_space = Coworking_space.find(params[:coworking_space_id])
+    @coworking_space = CoworkingSpace.find(params[:coworking_space_id])
   end
 
   def create
-    @coworking_booking = Coworking_booking.new(coworking_booking_params) # creating the object with the filteredform data
-    @coworking_space = Coworking_space.find(params[:coworking_space_id]) # grabbing the coworking_space by the id in params
+    @coworking_booking = Booking.new(coworking_booking_params) # creating the object with the filteredform data
+    @coworking_space = CoworkingSpace.find(params[:coworking_space_id]) # grabbing the coworking_space by the id in params
     @coworking_booking.user = @coworking_space # setting the coworking_space on the booking (saving the relationship)
     @coworking_booking.save  # saving the object
     redirect_to coworking_space_path(@coworking_space) # redirecting back to the coworking show path
@@ -20,7 +20,6 @@ class BookingsController < ApplicationController
   end
 end
 
-end
 
 
 # coworking_bookings POST   /coworkings/:coworking_id/bookings(.:format)                                             bookings#create
