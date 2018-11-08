3bece20d00792d291a01c975cbcde64dc84c717c
Shawkat Shami <shawkat.shami7@gmail.com>
2018-11-08 12:00:48 +0800

    booking methods created for new booking & booking creation

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,9 @@
 class Booking < ApplicationRecord
   belongs_to :coworking_space
   belongs_to :user
+
+  validates :coworking_space, presence: true
+  validates :user_id, presence: true
+  validates :start_datetime, :end_datetime, presence: true
+  validates :status, presence: true
 end
