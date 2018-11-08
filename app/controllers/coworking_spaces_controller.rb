db126d5804fa3b1e60f61744c2b7e64b9b0f0863
Merlin Hannemann <merlinhannemann@gmx.de>
2018-11-08 14:29:16 +0800

    updated the controller

--- app/controllers/coworking_spaces_controller.rb
+++ app/controllers/coworking_spaces_controller.rb
@@ -5,18 +5,18 @@ class CoworkingSpacesController < ApplicationController
   end
 
   def new
-    @coworking_spaces = CoworkingSpace.new
+    @coworking_space = CoworkingSpace.new
   end
 
   def create
-    @coworking_spaces = CoworkingSpace.new(params_coworking_spaces)
-    @coworking_spaces.save
-    redirect_to coworking_spaces_path
+    @coworking_space = CoworkingSpace.new(params_coworking_space)
+    @coworking_space.save
+    redirect_to coworking_space_path
   end
 
   private
 
-  def params_coworking_spaces
-    params.require(:coworking_spaces).permit(:location, :capacity, :services, :price, :name)
+  def params_coworking_space
+    params.require(:coworking_space).permit(:location, :capacity, :services, :price, :name)
   end
 end
