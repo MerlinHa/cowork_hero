29a085d5d21cfd482b540306bce35cc4d771bc9a
julesjamet <jules.jamet@gmail.com>
2018-11-08 11:45:24 +0800

    add new and create actions inside the controller + private params actions

--- app/controllers/coworking_spaces_controller.rb
+++ app/controllers/coworking_spaces_controller.rb
@@ -1,2 +1,17 @@
 class CoworkingSpacesController < ApplicationController
+  def new
+    @coworking = Coworking.new
+  end
+
+  def create
+    @coworking = Coworking.new(params_coworkings)
+    @coworking.save
+    redirect_to coworkings_path
+  end
+
+  private
+
+  def params_coworkings
+    params.require(:coworking).permit(:location, :capacity, :services, :price, :name)
+  end
 end
