0df8353ec493e2bd333ee551777eddbe3a8cebac
Merlin Hannemann <merlinhannemann@gmx.de>
2018-11-09 11:02:13 +0800

    added columns to CoworkingSpaces table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_07_085136) do
+ActiveRecord::Schema.define(version: 2018_11_09_025806) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,14 @@ ActiveRecord::Schema.define(version: 2018_11_07_085136) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.float "latitude"
+    t.float "longitude"
+    t.string "short_desc"
+    t.string "long_desc"
+    t.string "address"
+    t.integer "zip"
+    t.string "city"
+    t.string "country"
     t.index ["user_id"], name: "index_coworking_spaces_on_user_id"
   end
 
